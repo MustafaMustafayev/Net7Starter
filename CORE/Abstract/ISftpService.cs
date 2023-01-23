a51eb6b44483d3520c5564ec169997d59c833a7f
Hamid Musayev <hemidvsmusayev@gmail.com>
2023-01-23 21:54:34 +0400

    automapper generator added & some sftp service code refactored

--- CORE/Abstract/ISftpService.cs
+++ CORE/Abstract/ISftpService.cs
@@ -1,15 +1,13 @@
-﻿
-using DTO.Sftp;
+﻿using DTO.Sftp;
 using Microsoft.AspNetCore.Http;
 
-namespace CORE.Abstract
+namespace CORE.Abstract;
+
+public interface ISftpService
 {
-    public interface ISftpService
-    {
-        List<DirectoryInformation> GetDirectoryInformation(string path);
-        void UploadFile(string filePath, string fileName, IFormFile formFile);
-        void DeleteFile(string filePath);
-        byte[] ReadImage(string filePath);
-        byte[] CompressImage(int jpegQuality, byte[] data);
-    }
+    List<DirectoryInformation> GetDirectoryInformation(string path);
+    void UploadFile(string filePath, string fileName, IFormFile formFile);
+    void DeleteFile(string filePath);
+    byte[] ReadImage(string filePath);
+    byte[] CompressImage(int jpegQuality, byte[] data);
 }
--- CORE/Concrete/SftpService.cs
+++ CORE/Concrete/SftpService.cs
@@ -1,154 +1,129 @@
-﻿using CORE.Abstract;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using CORE.Abstract;
 using CORE.Config;
 using DTO.Sftp;
 using Microsoft.AspNetCore.Http;
 using Renci.SshNet;
 using Renci.SshNet.Sftp;
-using System.Drawing;
-using System.Drawing.Imaging;
 using ConnectionInfo = Renci.SshNet.ConnectionInfo;
 
-namespace CORE.Concrete
+namespace CORE.Concrete;
+
+public class SftpService : ISftpService
 {
-    public class SftpService : ISftpService
+    private readonly ConfigSettings _configSettings;
+
+    public SftpService(ConfigSettings configSettings)
     {
-        public ConfigSettings _configSettings;
-        public SftpService(ConfigSettings configSettings)
-        {
-            _configSettings = configSettings;
-        }
-        public List<DirectoryInformation> GetDirectoryInformation(string path)
-        {
-            if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path))
-            {
-                path = string.Empty;
-            }
+        _configSettings = configSettings;
+    }
 
-            List<DirectoryInformation> directoryInfos = new List<DirectoryInformation>();
-            var connectionInfo = GetConnectionInfo();
-            using (var sftp = new SftpClient(connectionInfo))
-            {
-                sftp.Connect();
-                if (sftp.IsConnected)
+    public List<DirectoryInformation> GetDirectoryInformation(string path)
+    {
+        if (string.IsNullOrEmpty(path) || string.IsNullOrWhiteSpace(path)) path = string.Empty;
+
+        var directoryInfos = new List<DirectoryInformation>();
+        var connectionInfo = GetConnectionInfo();
+        using (var sftp = new SftpClient(connectionInfo))
+        {
+            sftp.Connect();
+            if (!sftp.IsConnected) return directoryInfos.OrderBy(m => m.Name).ThenBy(m => !m.IsDirectory).ToList();
+
+            var realPath = ("/" + path).Replace("//", "/");
+            sftp.ChangeDirectory(realPath);
+            List<SftpFile> directories = sftp.ListDirectory(realPath).ToList();
+            directoryInfos.AddRange(from sftpFile in directories
+                let fileName = sftpFile.Name
+                let fileParts = fileName.Split('.')
+                where fileName != "." && fileName != ".."
+                let isAvaliable = sftpFile.IsDirectory || fileParts[^1] == "mp4"
+                where isAvaliable
+                let subDirectory = sftpFile.FullName.StartsWith("/") && sftpFile.FullName.Length > 0
+                    ? sftpFile.FullName.Remove(0, 1)
+                    : sftpFile.FullName
+                select new DirectoryInformation
                 {
-                    string realPath = ("/" + path).Replace("//", "/");
-                    sftp.ChangeDirectory(realPath);
-                    List<SftpFile> directories = sftp.ListDirectory(realPath).ToList();
-                    foreach (SftpFile sftpFile in directories)
-                    {
-                        string fileName = sftpFile.Name;
-                        string[] fileParts = fileName.Split('.');
-                        if (fileName != "." && fileName != "..")
-                        {
-                            bool isAvaliable = sftpFile.IsDirectory ? true : fileParts[fileParts.Length - 1] == "mp4" ? true : false;
-                            if (isAvaliable)
-                            {
-                                string subDirectory = sftpFile.FullName.StartsWith("/") && sftpFile.FullName.Length > 0 ? sftpFile.FullName.Remove(0, 1) : sftpFile.FullName;
-
-                                DirectoryInformation directoryInfo = new DirectoryInformation()
-                                {
-                                    Name = sftpFile.Name,
-                                    Length = sftpFile.Length.ToString(),
-                                    Path = subDirectory,
-                                    CreatedAt = sftpFile.LastWriteTime.ToString("dd-MMM-yyyy HH:mm"),
-                                    IsDirectory = sftpFile.IsDirectory
-                                };
-                                directoryInfos.Add(directoryInfo);
-                            }
-                        }
-                    }
-                }
-            }
-
-            return directoryInfos.OrderBy(m => m.Name).OrderBy(m => !m.IsDirectory).ToList();
+                    Name = sftpFile.Name,
+                    Length = sftpFile.Length.ToString(),
+                    Path = subDirectory,
+                    CreatedAt = sftpFile.LastWriteTime.ToString("dd-MMM-yyyy HH:mm"),
+                    IsDirectory = sftpFile.IsDirectory
+                });
         }
 
-        public void UploadFile(string filePath, string fileName, IFormFile formFile)
+        return directoryInfos.OrderBy(m => m.Name).ThenBy(m => !m.IsDirectory).ToList();
+    }
+
+    public void UploadFile(string filePath, string fileName, IFormFile formFile)
+    {
+        var connectionInfo = GetConnectionInfo();
+        using var sftp = new SftpClient(connectionInfo);
+        sftp.Connect();
+        if (!sftp.IsConnected) return;
+
+        sftp.ChangeDirectory(filePath);
+        using (var ms = new MemoryStream())
         {
-            var connectionInfo = GetConnectionInfo();
-            using (var sftp = new SftpClient(connectionInfo))
-            {
-                sftp.Connect();
-                if (sftp.IsConnected)
-                {
-                    sftp.ChangeDirectory(filePath);
-                    using (var ms = new MemoryStream())
-                    {
-                        formFile.CopyTo(ms);
-                        var fileBytes = ms.ToArray();
-                        sftp.WriteAllBytes(fileName, fileBytes);
-                    }
-
-                    sftp.Disconnect();
-                }
-            }
+            formFile.CopyTo(ms);
+            var fileBytes = ms.ToArray();
+            sftp.WriteAllBytes(fileName, fileBytes);
         }
 
-        private ConnectionInfo GetConnectionInfo()
+        sftp.Disconnect();
+    }
+
+    public void DeleteFile(string filePath)
+    {
+        var connectionInfo = GetConnectionInfo();
+        using var sftp = new SftpClient(connectionInfo);
+
+        sftp.Connect();
+        if (!sftp.IsConnected) return;
+
+        sftp.ChangeDirectory(filePath);
+
+        if (sftp.Exists(filePath)) sftp.Delete(filePath);
+    }
+
+    public byte[] ReadImage(string filePath)
+    {
+        byte[] imageBytes = { };
+        var connectionInfo = GetConnectionInfo();
+        using var sftp = new SftpClient(connectionInfo);
+        sftp.Connect();
+
+        if (!sftp.IsConnected) return imageBytes;
+
+        sftp.ChangeDirectory(filePath);
+
+        if (string.IsNullOrEmpty(filePath)) return imageBytes;
+
+        if (sftp.Exists(filePath))
+            imageBytes = sftp.ReadAllBytes(filePath);
+
+        return imageBytes;
+    }
+
+    public byte[] CompressImage(int jpegQuality, byte[] data)
+    {
+        using var inputStream = new MemoryStream(data);
+        using var image = Image.FromStream(inputStream);
+        var jpegEncoder = ImageCodecInfo.GetImageDecoders()
+            .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+        var encoderParameters = new EncoderParameters(1);
+        encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, jpegQuality);
+        byte[] outputBytes = null;
+        using (var outputStream = new MemoryStream())
         {
-            return new Renci.SshNet.ConnectionInfo(_configSettings.SftpSettings.IP, _configSettings.SftpSettings.UserName, new PasswordAuthenticationMethod(_configSettings.SftpSettings.UserName, _configSettings.SftpSettings.Password));
+            image.Save(outputStream, jpegEncoder, encoderParameters);
+            return outputStream.ToArray();
         }
+    }
 
-        public void DeleteFile(string filePath)
-        {
-            var connectionInfo = GetConnectionInfo();
-            using (var sftp = new SftpClient(connectionInfo))
-            {
-                sftp.Connect();
-                if (sftp.IsConnected)
-                {
-                    sftp.ChangeDirectory(filePath);
-
-
-                    if (sftp.Exists(filePath))
-                    {
-                        sftp.Delete(filePath);
-                    }
-                }
-            }
-        }
-
-        public byte[] ReadImage(string filePath)
-        {
-            byte[] imageBytes = new byte[] { };
-            var connectionInfo = GetConnectionInfo();
-            using (var sftp = new SftpClient(connectionInfo))
-            {
-                sftp.Connect();
-                if (sftp.IsConnected)
-                {
-                    sftp.ChangeDirectory(filePath);
-
-                    if (!string.IsNullOrEmpty(filePath))
-                    {
-                        if (sftp.Exists(filePath))
-                        {
-                            imageBytes = sftp.ReadAllBytes(filePath);
-                        }
-                    }
-                }
-            }
-            return imageBytes;
-        }
-
-        public byte[] CompressImage(int jpegQuality, byte[] data)
-        {
-            using (MemoryStream inputStream = new MemoryStream(data))
-            {
-                using (Image image = Image.FromStream(inputStream))
-                {
-                    ImageCodecInfo jpegEncoder = ImageCodecInfo.GetImageDecoders()
-                        .First(c => c.FormatID == ImageFormat.Jpeg.Guid);
-                    var encoderParameters = new EncoderParameters(1);
-                    encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, jpegQuality);
-                    byte[] outputBytes = null;
-                    using (MemoryStream outputStream = new MemoryStream())
-                    {
-                        image.Save(outputStream, jpegEncoder, encoderParameters);
-                        return outputStream.ToArray();
-                    }
-                }
-            }
-        }
+    private ConnectionInfo GetConnectionInfo()
+    {
+        return new ConnectionInfo(_configSettings.SftpSettings.IP, _configSettings.SftpSettings.UserName,
+            new PasswordAuthenticationMethod(_configSettings.SftpSettings.UserName, _configSettings.SftpSettings.Password));
     }
 }
