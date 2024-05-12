47b3eadea27eadb8bd31f736c314b101d8674f9f
Kamil <jkamil@live.ru>
2024-05-12 20:16:09 +0400

    fix

---
+++ ENTITIES/Entities/Test/Test.cs
@@ -0,0 +1,8 @@
+﻿using ENTITIES.Entities.Generic;
+
+namespace ENTITIES.Entities.Test;
+public class Test : IEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+}
